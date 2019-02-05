e1780e33ccf8e6a839a697dd02a75910e6e85e35
vss-py <victorsantos.py@gmail.com>
2019-02-05 17:53:13 -0200

    adding decorator login required

--- user_example/urls.py
+++ user_example/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('registro', views.register, name='registro'),
     path('blog', views.blog, name='blog'),
+    path('error', views.blog, name='error'),
+    path('blog2', views.blog2, name='blog2'),
+    #path('error2', views.blog2, name='error2'),
 ]
--- user_example/views.py
+++ user_example/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 def index(request):
 	return render(request, 'user_example/index.html')
@@ -22,6 +23,12 @@ def register(request):
 	return render(request, 'registration/register.html', context)
 
 def blog(request):
-	return render(request, 'user_example/blog.html')
+	if not request.user.is_authenticated:
+		return render(request, 'user_example/error.html')
+	else:
+		return render(request, 'user_example/blog.html')
 
+@login_required(login_url='/accounts/login/')
+def blog2(request):
+	return render(request, 'user_example/blog2.html')
 #
